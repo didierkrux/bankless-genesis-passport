d0f47169fab91fe46168fbdb3eda30fa08411047
Didier Krux <didier@krux.co>
2023-01-19 20:48:27 +0000

    handle undefined case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main>
         <AspectRatio m="auto" w="500px" maxW="100%" ratio={1}>
-          <iframe src={`/nft.html${stateAddress ? `?address=${stateAddress}` : ''}`} width="100%" height="100%" />
+          <iframe src={`/nft.html${stateAddress?.length ? `?address=${stateAddress}` : ''}`} width="100%" height="100%" />
         </AspectRatio>
         <FormControl display="flex" alignItems="center" justifyContent="center" mt="8">
           <FormLabel color="burlywood" mb="0">
