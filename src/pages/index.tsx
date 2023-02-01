bcc5b87210a5c83401742e0eb1d89efbc72aaed9
Didier Krux <didier@krux.co>
2023-02-01 13:19:27 +0000

    fix full gauntlet demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,10 @@ export default function Home() {
   const { address } = useAccount()
   const [isDemo, setIsDemo] = useState(false)
 
-  const stateAddress = isDemo ? '0xBD19a3F0A9CaCE18513A1e2863d648D13975CB30' : address
+  const stateAddress = isDemo ? '0x34b515d8d5eb3f1828170c0ac992c1b539e6ca87' : address
   return (
     <>
       <Head />
-
       <main>
         <AspectRatio m="auto" w="500px" maxW="100%" ratio={1}>
           <iframe src={`/nft.html${stateAddress?.length ? `?address=${stateAddress}` : ''}`} width="100%" height="100%" />
