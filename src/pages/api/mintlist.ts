28bd569156eedc539ab4425adf5aba4b75c45cc0
Didier Krux <didier@krux.co>
2023-02-06 18:14:19 +0000

    add support for matic + tokenIDs

--- src/pages/api/mintlist.ts
+++ src/pages/api/mintlist.ts
@@ -4,6 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const ALCHEMY_KEY = 'OqVebafskIMfxHaVJNBdms7AmlzRaQ9X'
 
 const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`
+const baseURLMATIC = `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { hasAllNFTs, nfts } = req.query
@@ -14,11 +15,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const nftsArray = typeof nfts === 'string' ? nfts?.split(',').filter((a) => a.trim() !== '') : []
 
     for (const nftAddress of nftsArray) {
-      endpoints.push(`${baseURL}/getOwnersForCollection/?contractAddress=${nftAddress}`)
+      if (nftAddress.startsWith('matic')) {
+        const [, contractAddress, tokenId] = nftAddress.split('/')
+        const method = tokenId ? 'getOwnersForToken' : 'getOwnersForCollection'
+        endpoints.push(`${baseURLMATIC}/${method}/?contractAddress=${contractAddress}${tokenId ? `&tokenId=${tokenId}` : ''}`)
+      } else {
+        const [contractAddress, tokenId] = nftAddress.split('/')
+        const method = tokenId ? 'getOwnersForToken' : 'getOwnersForCollection'
+        endpoints.push(`${baseURL}/${method}/?contractAddress=${contractAddress}${tokenId ? `&tokenId=${tokenId}` : ''}`)
+      }
     }
     console.log(endpoints)
 
-    const collections: { ownerAddresses: string[] }[] = await Promise.all(
+    const collections: { owners?: string[]; ownerAddresses?: string[] }[] = await Promise.all(
       endpoints.map(async (endpoint) => {
         const resp = await fetch(endpoint)
         return resp.json()
@@ -30,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     for (const collection of collections) {
       // console.log(collection.ownerAddresses)
       // add unique owners for each collection
-      collectionOwners.push(...new Set(collection.ownerAddresses))
+      collectionOwners.push(...new Set(collection.ownerAddresses || collection.owners))
     }
     console.log('collectionOwners', collectionOwners)
     console.log('count collectionOwners', collectionOwners.length)
