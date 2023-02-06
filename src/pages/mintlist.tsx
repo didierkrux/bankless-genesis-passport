53651f8ecdf4b1bd263569599346498c35c17301
Didier Krux <didier@krux.co>
2023-02-06 18:16:04 +0000

    add support for Bankless Academy lessons allowlist

--- src/pages/mintlist.tsx
+++ src/pages/mintlist.tsx
@@ -1,25 +1,44 @@
-import { Box, FormControl, FormLabel, Switch, Textarea, Text, Button, SimpleGrid } from '@chakra-ui/react'
+import { Box, FormControl, FormLabel, Switch, Textarea, Text, Button, SimpleGrid, Container } from '@chakra-ui/react'
 import { Head } from 'components/layout/Head'
 import { useEffect, useState } from 'react'
 const { CSVLink } = require('react-csv')
+import queryString from 'query-string'
 
-// Bankless Genesis NFT addresses
-const NFT_ADDRESSES = [
-  '0xe60a7e1a1ee79832f8f8042b0cffb2eaddb5e6c0',
-  '0x1041f479b21f9ba7c8a487877b61098842f1f25a',
-  '0xbf6cb74f15655865542668512b55668c35d7fc94',
-  '0xd19a5ee68e2ed7c19d509b6f4ecad7409e79ad58',
-  '0xbfb33f1e522f9c02633f16bacf3f4f24dc4b8755',
-]
+const NFT_ADDRESSES: any = {
+  // Bankless Genesis Podcast gauntlet
+  bankless_gauntlet: [
+    '0xe60a7e1a1ee79832f8f8042b0cffb2eaddb5e6c0',
+    '0x1041f479b21f9ba7c8a487877b61098842f1f25a',
+    '0xbf6cb74f15655865542668512b55668c35d7fc94',
+    '0xd19a5ee68e2ed7c19d509b6f4ecad7409e79ad58',
+    '0xbfb33f1e522f9c02633f16bacf3f4f24dc4b8755',
+  ],
+  // Bankless Academy
+  bankless_academy: [
+    'matic/0x60576a64851c5b42e8c57e3e4a5cf3cf4eeb2ed6/2561',
+    'matic/0x60576a64851c5b42e8c57e3e4a5cf3cf4eeb2ed6/2562',
+    'matic/0x60576a64851c5b42e8c57e3e4a5cf3cf4eeb2ed6/2563',
+    'matic/0x60576a64851c5b42e8c57e3e4a5cf3cf4eeb2ed6/2564',
+    'matic/0x60576a64851c5b42e8c57e3e4a5cf3cf4eeb2ed6/2565',
+    'matic/0x60576a64851c5b42e8c57e3e4a5cf3cf4eeb2ed6/2608',
+    'matic/0x60576a64851c5b42e8c57e3e4a5cf3cf4eeb2ed6/14611',
+  ],
+}
+
+const DEFAULT_COLLECTION = 'bankless_gauntlet'
 
 export default function Home() {
-  const [nfts, setNFTs] = useState(NFT_ADDRESSES)
+  const collection =
+    typeof window !== 'undefined' ? (queryString.parse(window.location.search)?.nfts || DEFAULT_COLLECTION)?.toString() : DEFAULT_COLLECTION
+  const initialNFTs: string[] = collection in NFT_ADDRESSES ? NFT_ADDRESSES[collection] : []
+  const [nfts, setNFTs] = useState<string[]>(initialNFTs)
   const [mintList, setMintList] = useState<string[]>([])
   const [hasAllNFTs, setHasAllNFTs] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const getMintList = async () => {
-      setMintList(['...'])
+      setIsLoading(true)
       try {
         const res = await fetch(`/api/mintlist?hasAllNFTs=${hasAllNFTs}&nfts=${nfts.join(',')}`)
         if (res.status === 200) {
@@ -27,12 +46,16 @@ export default function Home() {
           console.log(json)
           if (json) setMintList(json)
         } else setMintList(['error, please refresh'])
-      } catch (_error) {}
+        setIsLoading(false)
+      } catch (_error) {
+        setIsLoading(false)
+      }
     }
-    getMintList()
+    if (!isLoading) getMintList()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [hasAllNFTs, nfts])
 
-  let handleInputChange = (e: any) => {
+  const handleInputChange = (e: any) => {
     const inputValue = e.target.value
     setNFTs(inputValue.split('\n'))
   }
@@ -45,7 +68,7 @@ export default function Home() {
     <>
       <Head />
       <main>
-        <SimpleGrid minChildWidth="420px" columns={2} spacing={4}>
+        <SimpleGrid minChildWidth="500px" columns={2} spacing={4}>
           <Box justifySelf="center" w="100%" p="4">
             <Text align="center" fontSize="xl" mb="2">
               NFTs (ERC-721 on Mainnet)
@@ -56,10 +79,10 @@ export default function Home() {
             <Text align="center" fontSize="xl" mb="2">
               {`Mintlist (${hasAllNFTsLabel}): `}
               <Text as="span" color="burlywood">
-                {`${mintList.join() === '...' ? mintList.join() : mintList.length} unique addresses`}
+                {`${isLoading ? '...' : mintList.length} unique addresses`}
               </Text>
             </Text>
-            <Textarea w="100%" h="600px" value={Array.isArray(mintList) ? mintList?.join('\n') : ''}></Textarea>
+            <Textarea w="100%" h="600px" value={isLoading ? 'loading ...' : Array.isArray(mintList) ? mintList?.join('\n') : ''}></Textarea>
           </Box>
           <Box justifySelf="center">
             <FormControl display="flex" alignItems="center" justifyContent="center">
